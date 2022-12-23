e71f0bcb344f6190eb0460e2a514bd8317d949f8
rgoldsmith33703 <rgolds33703@outlook.com>
2022-12-22 21:35:02 -0600

    add books to page function

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 let myLibrary = [];
 
-function Book() {
-    // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(title, author, pages, read = false) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
+
+function displayBooksOnPage() {
+    const books = document.querySelector('.books');
+    myLibrary.forEach(myLibrary => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    books.appendChild(card);
+
+    for (let key in myLibrary) {
+        const para = document.createElement('p');
+        para.textContent = (`${key}: ${myLibrary[key]}`);
+        card.appendChild(para);
+    }})}
